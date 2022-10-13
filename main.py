403aca9928cfbea777d591d1c639e3018695173d
electro <electrocoder@gmail.com>
2022-10-13 22:20:27 +0300

    uu

--- main.py
+++ main.py
@@ -1,38 +1,32 @@
-from tkinter import *
-from tkinter import ttk
-
-def calculate(*args):
-    try:
-        value = float(feet.get())
-        meters.set(int(0.3048 * value * 10000.0 + 0.5)/10000.0)
-    except ValueError:
-        pass
+from tkinter import Tk, BOTTOM, LEFT, HORIZONTAL, Listbox, BOTH
+from tkinter.ttk import Style, Frame, Button, Entry, PanedWindow
 
 root = Tk()
-root.title("Feet to Meters")
+root.title('PanedWindow Demo')
+root.geometry('300x200')
 
-mainframe = ttk.Frame(root, padding="3 3 12 12")
-mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
-root.columnconfigure(0, weight=1)
-root.rowconfigure(0, weight=1)
+style = Style()
+style.theme_use('classic')
 
-feet = StringVar()
-feet_entry = ttk.Entry(mainframe, width=7, textvariable=feet)
-feet_entry.grid(column=2, row=1, sticky=(W, E))
+connect_frame = Frame(root)
+connect_frame.pack()
 
-meters = StringVar()
-ttk.Label(mainframe, textvariable=meters).grid(column=2, row=2, sticky=(W, E))
+redbutton = Button(connect_frame, text="Red")
+redbutton.pack(side=LEFT)
 
-ttk.Button(mainframe, text="Calculate", command=calculate).grid(column=3, row=3, sticky=W)
+E1 = Entry(connect_frame)
+E1.pack()
 
-ttk.Label(mainframe, text="feet").grid(column=3, row=1, sticky=W)
-ttk.Label(mainframe, text="is equivalent to").grid(column=1, row=2, sticky=E)
-ttk.Label(mainframe, text="meters").grid(column=3, row=2, sticky=W)
+pw = PanedWindow(orient=HORIZONTAL)
 
-for child in mainframe.winfo_children(): 
-    child.grid_configure(padx=5, pady=5)
+B1 = Button(root, text="show")
+B1.pack()
+pw.add(B1)
 
-feet_entry.focus()
-root.bind("<Return>", calculate)
+right_list = Listbox(root)
+right_list.pack(side=LEFT)
+pw.add(right_list)
+
+pw.pack(fill=BOTH, expand=True)
 
 root.mainloop()
