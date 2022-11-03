babda4742faa1dbf769fe97270be85a0fd412c53
meseiot <info@mesemekatronik.com>
2022-11-03 21:36:44 +0300

    up

--- new_connect.py
+++ new_connect.py
@@ -5,18 +5,53 @@ def new_connect_window(master):
     window = tk.Toplevel(master)
     window.grab_set()
     window.title("MQTT Client New Connect")
-    window.geometry("400x300")
+    # window.geometry("400x300")
 
-    self.button_connect = tk.Button(self,
-                                    text="Connect",
-                                    command=main_window_self.button_connect)
-    self.button_connect.pack()
+    row = 0
+    column = 0
 
-    self.button_disconnect = tk.Button(self,
-                                       text="Dur",
-                                       command=main_window_self.button_disconnect)
-    self.button_disconnect.pack()
+    label_broker = tk.Label(window, text="Broker")
+    label_broker.grid(row=row, column=column)
+    column += 1
+    entry_broker_text = tk.StringVar()
+    entry_broker = tk.Entry(window, textvariable=entry_broker_text)
+    entry_broker.grid(row=row, column=column)
 
-    self.entry_msg_text = tk.StringVar()
-    self.entry_msg = tk.Entry(self, textvariable=self.entry_msg_text)
-    self.entry_msg.pack()
+    row += 1
+    column = 0
+
+    label_port = tk.Label(window, text="Port")
+    label_port.grid(row=row, column=column)
+    column += 1
+    entry_port_text = tk.StringVar()
+    entry_port = tk.Entry(window, textvariable=entry_port_text)
+    entry_port.grid(row=row, column=column)
+
+    row += 1
+    column = 0
+
+    label_username = tk.Label(window, text="Username")
+    label_username.grid(row=row, column=column)
+    column += 1
+    entry_username_text = tk.StringVar()
+    entry_username = tk.Entry(window, textvariable=entry_username_text)
+    entry_username.grid(row=row, column=column)
+
+    row += 1
+    column = 0
+
+    label_password = tk.Label(window, text="Password")
+    label_password.grid(row=row, column=column)
+    column += 1
+    entry_password_text = tk.StringVar()
+    entry_password = tk.Entry(window, textvariable=entry_password_text)
+    entry_password.grid(row=row, column=column)
+
+    row += 1
+    column = 0
+    button_cancel = tk.Button(window, text="Cancel")
+    button_cancel.grid(row=row, column=column)
+    column += 1
+    button_save = tk.Button(window, text="Save")
+    button_save.grid(row=row, column=column)
+
