fd4506ec3b6026c5fa516b9a4f7d647426402a53
meseiot <info@mesemekatronik.com>
2022-11-07 21:22:41 +0300

    up

--- del.py
+++ del.py
@@ -1,24 +1,51 @@
-# import required modules
-from tkinter import *
+#!/usr/bin/python3
+import tkinter as tk
 
-tkWindow = Tk()
-tkWindow.geometry('500x500')
-tkWindow.title('Example Utility')
 
-scrollbar = Scrollbar(tkWindow, orient="vertical")
+class PygubuApp:
+    def __init__(self, master=None):
+        # build ui
+        frame1 = tk.Frame(master)
+        frame1.configure(height=200, width=200)
+        label3 = tk.Label(frame1)
+        label3.configure(text='Broker')
+        label3.grid(column=0, row=0)
+        entry3 = tk.Entry(frame1)
+        entry3.grid(column=1, row=0)
+        button1 = tk.Button(frame1)
+        button1.configure(text='Connect')
+        button1.grid(column=2, row=0)
+        button2 = tk.Button(frame1)
+        button2.configure(text='Disconnect')
+        button2.grid(column=3, row=0)
+        label4 = tk.Label(frame1)
+        label4.configure(text='Publish')
+        label4.grid(column=0, row=1)
+        entry4 = tk.Entry(frame1)
+        entry4.grid(column=1, row=1)
+        button3 = tk.Button(frame1)
+        button3.configure(text='Publish')
+        button3.grid(column=2, row=1)
+        listbox1 = tk.Listbox(frame1)
+        listbox1.grid(column=1, row=2)
+        label5 = tk.Label(frame1)
+        label5.configure(text='Subscribe')
+        label5.grid(column=0, row=3)
+        listbox2 = tk.Listbox(frame1)
+        listbox2.grid(column=1, row=3)
+        button4 = tk.Button(frame1)
+        button4.configure(text='button4')
+        button4.grid(column=2, row=3)
+        frame1.grid(column=0, row=0)
 
-SelectuserLabel = Label(tkWindow, text="Select").grid(row=0, column=0)
-test = Listbox(tkWindow, width=10, height=5, font=("Helvetica", 14))
-test.insert(END, 1)
-test.insert(END, 2)
-test.insert(END, 3)
-test.insert(END, 4)
-test.insert(END, 5)
-test.insert(END, 6)
-test.insert(END, 7)
-test.insert(END, 8)
-test.grid(row=0, column=1)
-scrollbar.config(command=test.yview)
-scrollbar.grid(row=0, column=2, sticky='ns')
+        # Main widget
+        self.mainwindow = frame1
 
-tkWindow.mainloop()
+    def run(self):
+        self.mainwindow.mainloop()
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = PygubuApp(root)
+    app.run()
